67ae32f82f3356fbb4c53868e38adcd4629c311f
h-clef <matthew.rumizen@gmail.com>
2020-06-23 15:17:30 -0400

    completed first db upload script

--- extract_coords.py
+++ extract_coords.py
@@ -1,27 +1,80 @@
 # extract_coords
-# A test module for turning .osm 'ways' into global coordinate paths.
-# Automatically uploads 'sample_map.osm' from current directory.
-
-from typing import List, Tuple
+# A module for turning Garmin .gpx 'trk's into global coordinate paths
+# and then formatting and uploading the data to a MySQL server.
+from re import sub
 import xml.etree.ElementTree as ET
+import mysql.connector as MSC
 
-with open('sample_map.osm', 'r') as sample_map:
-    tree = ET.parse(sample_map)
-    all_nodes = [ n for n in tree.getroot() if n.tag == 'node' ]
 
-def extract_coords(way) -> List[Tuple[float,float]]:
-    """Takes a 'way' element from an .osm file and returns
-    a list of (latitude, longitude) coordinates."""
+with open('activity1.gpx', 'r') as gpx_route:
+    tree = ET.parse(gpx_route)
+    root = tree.getroot()
 
-    # assert type(way) == 'xml.etree.ElementTree.Element'
 
-    node_ids = [ n.attrib['ref'] for n in way if n.tag == 'nd' ]
-    coords = []
+def upload_data(pwd: str) -> None:
+    print("Starting upload script.")
 
-    for node_id in node_ids:
-        for n in all_nodes:
-            if n.attrib['id'] == node_id:
-                coords.append( (n.attrib['lat'], n.attrib['lon']) )
-                break
+    cnx = MSC.connect(user='matthew', password=pwd, host='10.0.0.129', database='trackJog')
+    cursor = cnx.cursor()
+    print("Connected to trackJog database.")
 
-    return coords
+    assert root[2].tag == '{http://www.topografix.com/GPX/1/0}trk'
+    trk = root[2]
+    trksegs = list(trk)
+    print(f"{len(trksegs)} segments found.")
+
+    for i,trkseg in enumerate(trksegs):
+
+        trkpts = list(trkseg)
+        assert len(trkpts) >= 2
+        print(f"Uploading segment {i+1}: {len(trkpts)} points to add.")
+
+        for j,trkpt in enumerate(trkpts):
+
+            DT = sub('T', ' ', trkpt[0].text[:-1])
+            LAT = float( trkpt.attrib['lat'] )
+            LON = float( trkpt.attrib['lon'] )
+
+            data_point = (LAT, LON, DT)
+            add_point = ("INSERT INTO points_test "
+                         "(latitude, longitude, time) "
+                         "VALUES (%s, %s, %s)"
+                        )
+
+            cursor.execute(add_point, data_point)
+            if j > 0 and j%100 == 0: print(f"Added points {j-100+1}-{j}.")
+
+
+            if j == 0 or j == len(trkpts)-1:
+
+                TIME = sub('T',' ',DT[:-1])
+                ID = cursor.lastrowid
+
+                if j == 0:
+                    flag_point = ('start', ID)
+                    START_TIME = TIME
+                    START_ID = ID
+                else:
+                    flag_point = ('stop', ID)
+                    STOP_TIME = TIME
+                    STOP_ID = ID
+
+                update_point = ("UPDATE points_test "
+                                "SET flag = %s "
+                                "WHERE id = %s"
+                                )
+
+                cursor.execute(update_point, flag_point)
+
+
+        data_route = (START_TIME, STOP_TIME, START_ID, STOP_ID)
+        add_route = ("INSERT INTO routes_test "
+                     "(start_time, stop_time, start_point, stop_point) "
+                     "VALUES (%s, %s, %s, %s)"
+                    )
+
+        cursor.execute(add_route, data_route)
+        print(f"Finished uploading segment {i+1}.")
+
+    cnx.commit()
+    print("Upload complete.")
--- upload_new_data.sh
+++ upload_new_data.sh
@@ -4,3 +4,6 @@
 # MySQL server.
 
 echo "Running script..."
+
+# find ACTIVITY_DATA/ -newermt '2020-06-10 16:09:34'
+# gpsbabel -i garmin_fit -f $FILE -o gpx -F {?}
